969c7130dbea16f2cf158104617b6604dcd55d1d
jonessilva <44277774+jonessilva@users.noreply.github.com>
2019-10-02 20:35:28 -0300

    inicio_proj_estoque
    criacao de formulario

---
+++ model/estoque/estoque.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Estoque{
+    public $nome;
+    public $marca;
+    public $preco;
+    public $
+}
+
+
+
+
+
+
+
---
+++ view/estoque/estoque.Main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt=BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" name="nome_produto" id=""><br>
+        <input type="text" name="marca" id=""><br>
+        <input type="number" name="preco" id=""><br>
+        <input type="number" name="quantia" id=""><br>
+        <input type="number" name="quantia_minima" id=""><br>
+        <input type="submit" value="Cadastrar">
+
+
+    </form>
+</body>
+
+</html>
